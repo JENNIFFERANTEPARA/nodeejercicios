152670fc4c7d8d6f7820884e993bc6c1a0fef664
jenniffer <prueba2022jenniffer@hotmail.com>
2023-06-26 02:03:56 -0500

    coneccion a base de dato

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 'use estric'
+const mongoose = require('mongoose');//CONECTARSE A LA BASE
 const app = require('./app');
 
 const port = 3700;
 
-app.listen(port,()=>{
-console.log("Servidor de ejemplo ejecutando en"+port);
-});
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/databases',{ useNewUrlParser:true, useUnifiedTopology:true})
+        .then(() => {
+            console.log('Conexion a la base de datos establecida con exito');
+
+            //Crear servidor
+           var server = app.listen(port,()=>{
+               console.log("Servidor corriendo correctamente en la url: http://localhost:" +port);
+                                            });
+    })
+    .catch(err => console.log(err));
+
+
+//app.listen(port,()=>{
+//console.log("Servidor de ejemplo ejecutando en" +port);
+//});
 
